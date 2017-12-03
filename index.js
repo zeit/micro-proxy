90f7b0739f86bf2f078823250bcc843a0916074e
Dima Paloskin <dima@paloskin.me>
2017-12-03 20:28:59 +0300

    Move back passing headers

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ async function proxyRequest (req, res, dest) {
   const newUrl = resolve(dest, req.url)
   const proxyRes = await fetch(newUrl, {
     method: req.method,
+    headers: req.headers,
     body: req,
     compress: false
   })
