6e7b5265fe38e3cf7d426f39eb944969623effb7
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-12-03 00:52:18 -0800

    Add all the test cases.

--- test/util.js
+++ test/util.js
@@ -1,8 +1,11 @@
 const micro = require('micro')
 const listen = require('test-listen')
 const fetch = require('node-fetch')
+const tempfile = require('tempfile')
+const { spawn } = require('child_process')
+const { writeFileSync, unlinkSync } = require('fs')
 
-exports.createInfoServer = async () => {
+exports.createInfoServer = async (port) => {
   const server = micro(async (req) => {
     const { method, headers, url } = req
     const body = await micro.text(req)
@@ -14,7 +17,7 @@ exports.createInfoServer = async () => {
     }
   })
 
-  await listen(server)
+  await listen(server, port)
 
   return {
     port: server.address().port,
@@ -33,4 +36,27 @@ exports.fetchProxy = async (proxy, path, options) => {
 
   const data = await res.json()
   return { data, res }
+}
+
+exports.startProxyCLI = async (rules, args = []) => {
+  const configFile = tempfile('.json')
+  writeFileSync(configFile, JSON.stringify({
+    rules
+  }))
+
+  const proxy = spawn('./bin/micro-proxy', ['-r', configFile, ...args])
+  proxy.stderr.pipe(process.stderr)
+
+  return new Promise((resolve, reject) => {
+    proxy.stdout.on('data', (d) => {
+      if (/Ready/.test(d.toString())) {
+        resolve({
+          close: () => {
+            proxy.kill()
+            unlinkSync(configFile)
+          }
+        })
+      }
+    })
+  })
 }
